b6ab05ce0db4801b64b66aee7340c04331d8bd15
ninjafella <henry@thefamilybishop.co.uk>
2024-04-26 04:55:28 +0100

    update to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2024, ninjafella"
 author = "ninjafella"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.0"
+release = "2.0.1"
 
 
 # -- General configuration ---------------------------------------------------
